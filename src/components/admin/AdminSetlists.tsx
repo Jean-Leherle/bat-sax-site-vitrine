35a33d3dd291f2be88852d8ee0cdefdf3944e7d0
jean.leherle <jean.leherle@autoprog.fr>
2026-05-06 22:20:36 +0200

    fix build

--- src/components/admin/AdminSetlists.tsx
+++ src/components/admin/AdminSetlists.tsx
@@ -1,6 +1,6 @@
 import { useState, useEffect } from "react";
 import { supabase } from "../../supabaseClient";
-import { type  Concert } from "./AdminConcerts";
+import {  type Concert } from "../../pages/Stages";
 import { type Track } from "./AdminTracks";
 
 type ConcertTrack = {
